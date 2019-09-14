ed2fd89f7baa214192a6fc7018894d4661ab1138
Mishra, AmitMishra <amit_mishra6@dell.com>
2019-09-14 22:16:57 +0530

    First

---
+++ AustinHackathon/AustinHackathon/Activity.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AustinHackathon
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Activity : ContentPage
+    {
+        public Activity()
+        {
+            InitializeComponent();
+            var browser = new WebView();
+            browser.Source = "file:///android_asset/TestWebPage.html"; 
+            Content = browser;
+        }
+    }
+}
---
+++ AustinHackathon/AustinHackathon/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AustinHackathon
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new SplashScreen());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app startsB
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
